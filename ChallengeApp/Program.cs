3a7f7a55d9fdff2b5286a08790e55036cae9bdb4
Wbolczyk <wbolczyk@gmail.com>
2023-06-05 17:13:52 +0200

    Zadanie domowe dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+int age = 33;
+bool isWomen = true;
+
+if (name != "Ewa" && age < 30 && isWomen == true)
+
+    Console.WriteLine("Kobieta poniżej 30 lat");
+
+else if (age < 17 && isWomen == false)
+
+    Console.WriteLine("Niepełnoletni mężczyzna");
+
+else if (name == "Ewa" && age == 33)
+
+    Console.WriteLine("Ewa lat 33");
+
+else
+
+    Console.WriteLine("Żaden warunek nie jest spełniony");
