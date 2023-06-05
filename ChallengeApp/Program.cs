a8f70172f3adf50a005f865b5a419142f8e85501
Wbolczyk <wbolczyk@gmail.com>
2023-06-05 20:07:44 +0200

    Zadanie domowe dzień 5

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,19 +1,76 @@
-﻿var name = "Ewa";
-int age = 33;
-bool isWomen = true;
+﻿int number = 451260067;
 
-if (name != "Ewa" && age < 30 && isWomen == true)
+//Przekształcenie liczby na formę tekstową
+string numberAsString = number.ToString();
 
-    Console.WriteLine("Kobieta poniżej 30 lat");
+//Utworzenie tablicy z poszczególnych znaków (liter) zawartych w liczbie
+char[] letters = numberAsString.ToArray();
 
-else if (age < 17 && isWomen == false)
+//Zmienne zawierające cyfry od 0 do 9
 
-    Console.WriteLine("Niepełnoletni mężczyzna");
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
 
-else if (name == "Ewa" && age == 33)
-
-    Console.WriteLine("Ewa lat 33");
-
-else
-
-    Console.WriteLine("Żaden warunek nie jest spełniony");
+foreach (char letter in letters)
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
+Console.WriteLine("Wpisana liczba to " + number);
+Console.WriteLine(" ");
+Console.WriteLine("Zawarte w liczbie cyfry oraz ich ilość:");
+Console.WriteLine(" ");
+Console.WriteLine("0 => " + counter0);
+Console.WriteLine("1 => " + counter1);
+Console.WriteLine("2 => " + counter2);
+Console.WriteLine("3 => " + counter3);
+Console.WriteLine("4 => " + counter4);
+Console.WriteLine("5 => " + counter5);
+Console.WriteLine("6 => " + counter6);
+Console.WriteLine("7 => " + counter7);
+Console.WriteLine("8 => " + counter8);
+Console.WriteLine("9 => " + counter9);
