4c1f5a2c1cc18d47006a4d222655756d15348e73
Wojciech Bolczyk <wbolczyk@hotmail.com>
2023-11-07 19:43:19 +0100

    Zadanie domowe dzień 15

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,12 +1,20 @@
 ﻿namespace ChallengeApp
 {
-    public class Employee : Person
+    public class Employee : IEmployee
     {
         private List<float> grades = new List<float>();
         public Employee(string name, string surname, char sex, int age)
-            : base(name, surname, sex, age)
-        { }
-              
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Sex = sex;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public char Sex { get; private set; }
+        public int Age { get; private set; }
+
         public void AddGrade(float grade)
         {
             if (grade >= 0 && grade <= 100)
